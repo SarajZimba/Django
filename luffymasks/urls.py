1887f9cd2b65525c899a4e4b4c1933b305528e5d
SarajZimba <sarajzimba@gmail.com>
2023-05-19 19:01:52 +0300

    form logic , cart functionality and while clicking payment order completion logic added using fetch and json

--- luffymasks/urls.py
+++ luffymasks/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('/view', views.view, name = 'view'),
     path('update_item/', views.updateItem, name = 'update_item'),
     path('', views.landingpage, name = 'home'),
+    path('process_order', views.processOrder, name = 'process_order'),
 ]
--- luffymasks/views.py
+++ luffymasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 # Create your views here.
 def home(request):
@@ -101,3 +102,29 @@ def landingpage(request):
     context = {'products': products}
     return render(request, "landingpage.html", context)
 
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        data = json.loads(request.body)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print('user is not logged in..')
+    return JsonResponse('Payment complete', safe=False)
